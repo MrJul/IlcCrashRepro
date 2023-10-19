ef4cf23b0b956223717819d987f3ea15da25958c
Julien Lebosquain <julien@lebosquain.net>
2023-10-19 19:41:59 +0200

    Initial commit

---
+++ MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace IlcCrashRepro;
+
+public partial class MainViewModel : ObservableObject
+{
+    [RelayCommand]
+    public void DoSomething(object param)
+        => Debug.WriteLine($"Hello {param}");
+}
